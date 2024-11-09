e07cf6606e079215f6d9cfb0e32f69a916fb617b
Colin Gillespie <csgillespie@gmail.com>
2024-11-09 17:23:18 +0000

    Initialise

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# 0.1.0 basic
+
+There is no news. This is a test file to determine if the assessments are working.
---
+++ R/add_one.R
@@ -0,0 +1,9 @@
+#' A Test function
+#'
+#' @param x An integer
+#'
+#' @return Nothing
+#' @export
+add_one = function(x) {
+  x + 1
+}
